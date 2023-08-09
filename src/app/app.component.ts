0678d8e0aabb17d9c22fa40fff81ff6dd1aabddf
Sergii Pavlov <4915906430009s@gmail.com>
2023-08-09 16:31:37 +0200

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-
-  
-  <div>
-    <h1>Список задач</h1>
-    <app-task-list [tasks]="tasks" (taskClicked)="onTaskClicked($event)"></app-task-list>
-    <app-create-task (click)="enterName(nameInput)"></app-create-task>
-  </div>
-
-  
-  `
+  templateUrl:`./app.component.html`
 })
 export class AppComponent {
   tasks: { id: number, title: string }[] = [
@@ -33,3 +23,6 @@ export class AppComponent {
     console.log('Задача выбрана:', task);
   }
 }
+
+
+// создть кнопку delete около каждой  строки в массиве
